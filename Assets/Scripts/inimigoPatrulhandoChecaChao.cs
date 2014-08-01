c30c55b2a8f0a4dedf48693ddea2a4761b25c4c2
Sílvio Medeiros <silviolqm@gmail.com>
2014-08-01 19:52:48 -0300

    Começou a construir o cenário. Consertou a patrulha do inimigo para não usar o renderizador da plataforma, mas sim um checaChao próprio do inimigo colocado à sua frente na altura do chão.

---
+++ Assets/Scripts/inimigoPatrulhandoChecaChao.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inimigoPatrulhandoChecaChao : MonoBehaviour {
+	bool andandoEsquerda = true;
+	public Transform checaChaoPatrulha;
+	public float velocidadePatrulha = 0.05f;
+	float chaoRaio = 0.2f;
+	bool noChao = false;
+	public LayerMask ehChao;
+
+
+	void FixedUpdate(){
+		float esquerda = (transform.position.x)+10f;
+		float direita = (transform.position.x)+10f;
+		Vector2 andaEsquerda = new Vector2(esquerda,transform.position.y);
+		Vector2 andaDireita = new Vector2(direita,transform.position.y);
+
+		noChao = Physics2D.OverlapCircle(checaChaoPatrulha.position, chaoRaio, ehChao);
+		if (noChao == true){ 
+		    if (andandoEsquerda == true)
+				transform.position = Vector2.MoveTowards(transform.position, -andaEsquerda, velocidadePatrulha);
+			else
+				transform.position = Vector2.MoveTowards(transform.position, andaDireita, velocidadePatrulha);
+		}
+		else if (noChao == false){
+			andandoEsquerda = !andandoEsquerda;
+			Flip();
+		}
+	}
+
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.tag == "Player")
+			StartCoroutine (Esperar(1f));
+	}
+	
+	
+	IEnumerator Esperar(float tempo) {
+		noChao = false;
+		//rigidbody2D.AddForce (new Vector2(5f,0));
+		yield return new WaitForSeconds(tempo); //espera um determinado tempo
+		rigidbody2D.velocity = Vector3.zero; //zera velocidades
+		rigidbody2D.angularVelocity = 0f;
+		noChao = true;
+	}
+	
+	
+	
+	void Flip (){
+		Vector2 aEscala = transform.localScale;
+		aEscala.x *= -1;
+		transform.localScale = aEscala;
+	}
+}
