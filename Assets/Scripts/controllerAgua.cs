7515321c40e763ba5ef706e5918102e6df24f386
Sílvio Medeiros <silviolqm@gmail.com>
2014-08-18 19:15:27 -0300

    Colliders na agua para apanhar.

---
+++ Assets/Scripts/controllerAgua.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllerAgua : MonoBehaviour {
+	float danoBasico = 50f;
+	float knockback = 0f;
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") { //checa a tag de quem colidiu pra ver se eh player
+			coll.rigidbody.velocity = Vector2.zero; //zera velocidades do player
+			coll.rigidbody.angularVelocity = 0f;
+			var posicaoRelativa = coll.contacts; //daqui em diante eh a parte que usa a normal para knockback 
+			GameObject jogador = coll.gameObject;
+			Debug.Log ("apanhou");
+			jogador.GetComponent<KitControllerBasico>().VariarLife ((ConfiguracoesGlobais.dificuldade * danoBasico)*-1);
+			jogador.GetComponent<KitControllerBasico>().Impulso(knockback*(-posicaoRelativa[0].normal[1]), 0); // usa normal[1] para jogar o player para os lados e evitar que o player possa ficar em cima do peixe
+		}
+	}
+}
