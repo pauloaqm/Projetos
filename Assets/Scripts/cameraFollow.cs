47ac5feade1fb3436948604dc2e8ad5cf9f5f17d
Sílvio Medeiros <silviolqm@gmail.com>
2014-08-18 18:42:23 -0300

    Adicionei novos tiles escuros e joguei fora os antigos que tinham linhas. Menos linhas na fase. Yay...

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class cameraFollow : MonoBehaviour 
 {
-	public static float xMargin = 0.3f;		// Distance in the x axis the player can move before the camera follows.
-	public float yMargin = 0.3f;		// Distance in the y axis the player can move before the camera follows.
+	public static float xMargin = 0.1f;		// Distance in the x axis the player can move before the camera follows.
+	public float yMargin = 0.1f;		// Distance in the y axis the player can move before the camera follows.
 	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
 	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
 	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
